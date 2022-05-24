b3794da867c9d36409b48fda99b7b87348bba44d
Sa Uk Chung <chungsauk95@gmail.com>
2022-05-23 21:53:24 -0400

    was able to print the data of chicago

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
+// API KEY 347ef9e5a11b48794065658c22cadcce
+
 // DEPENDENCIES
+var APIKey = '347ef9e5a11b48794065658c22cadcce';
 var cityInput = document.getElementById('searchInput');
 var searchButton = document.getElementById('searchButton');
+var city = "chicago";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)})
 
 
 // STATES
 
 // FUNCTIONS
-function lookupCity(event) {
-    var cityName = cityInput.value
-    console.log(cityName);
-};
 
 
 
 // USER INTERFACE
-searchButton.addEventListener("click", lookupCity);
