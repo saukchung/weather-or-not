20960231fc2aa021984b2bdf28c5bd76c7d18bb0
Sa Uk Chung <chungsauk95@gmail.com>
2022-05-23 23:15:07 -0400

    Added function renderCityData that creates the queryURL with the input city, and concats the h elements with their respective values. For the uvi, I had to fetch a queryURLuvi in order to access the uvi value in the access-by-lat-lon API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,58 @@
 // API KEY 347ef9e5a11b48794065658c22cadcce
 
 // DEPENDENCIES
-var APIKey = '347ef9e5a11b48794065658c22cadcce';
+//  construct the queryURL from the city input and the API key
+
 var cityInput = document.getElementById('searchInput');
 var searchButton = document.getElementById('searchButton');
-var city = "chicago";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data)})
+var APIKey = '347ef9e5a11b48794065658c22cadcce';
+var createName = document.querySelector("h2");
+var tempWindHumUV = document.querySelectorAll("h4");
 
 
 // STATES
 
 // FUNCTIONS
 
+function renderCityData() {
+    var city = cityInput.value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            createName.textContent += " " + data.name + " (" + moment().format('L') + ")";
+            tempWindHumUV[0].textContent += " " + data.main.temp + " K";
+            tempWindHumUV[1].textContent += " " + data.wind.speed + " MPH";
+            tempWindHumUV[2].textContent += " " + data.main.humidity + " %";
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+
+            var queryURLuvi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+            fetch(queryURLuvi)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data2){
+                    tempWindHumUV[3].textContent += " " + data2.current.uvi;
+                })
+
+            
+            
+
+
+
+
+        
+        });
+}
 
 
 // USER INTERFACE
+
+searchButton.addEventListener("click", function() {
+    renderCityData();
+}
+)
